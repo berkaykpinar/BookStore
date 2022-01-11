438c493e5fc9349fd1cec77e9d62caa4e6da1377
brky <uyelikicin189@gmail.com>
2022-01-11 18:49:45 +0300

    added

--- book-store/src/index.js
+++ book-store/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+//redux eklenecek
 ReactDOM.render(
   <BrowserRouter>
     <App />
